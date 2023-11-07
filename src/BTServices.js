842eda3c8c7d84cbb0788dbaf6affe8b9ce3ea4b
Tanko <john.tanko@falcontechnologies.com.gh>
2023-11-07 12:36:21 +0000

    fix

--- src/BTServices.js
+++ src/BTServices.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const BTServices = () => {
   return (
-    <section>
+    <section className="container mx-auto">
       <h1>Services</h1>
       <div className="flex flex-col md:flex-row gap-5 items-start px-12 pr-14 py-14 pt-20">
         <div className="flex-[2] lg:flex-[2] order-last animate-slide-in">
           <div className="">
             <h2 className="font-bold text-xl capitalize">heading</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis et porro architecto neque autem amet, eligendi est laboriosam natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos quos provident.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              et porro architecto neque autem amet, eligendi est laboriosam
+              natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos
+              quos provident.
+            </p>
           </div>
         </div>
         <div className="img-outline-left flex-1 order-first animate-slide-in2 z-10">
@@ -19,7 +24,12 @@ const BTServices = () => {
         <div className="flex-[2] lg:flex-[2] order-last md:order-first animate-slide-in">
           <div className="">
             <h2 className="font-bold text-xl capitalize">heading</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis et porro architecto neque autem amet, eligendi est laboriosam natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos quos provident.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              et porro architecto neque autem amet, eligendi est laboriosam
+              natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos
+              quos provident.
+            </p>
           </div>
         </div>
         <div className="img-outline-right flex-1 order-first md:order-last animate-slide-in2 z-10">
@@ -30,7 +40,12 @@ const BTServices = () => {
         <div className="flex-[2] lg:flex-[2] order-last animate-slide-in">
           <div className="">
             <h2 className="font-bold text-xl capitalize">heading</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis et porro architecto neque autem amet, eligendi est laboriosam natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos quos provident.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              et porro architecto neque autem amet, eligendi est laboriosam
+              natus ex, delectus nisi ea ducimus dolores quibusdam ipsum eos
+              quos provident.
+            </p>
           </div>
         </div>
         <div className="img-outline-left flex-1 order-first animate-slide-in2 z-10">
@@ -38,7 +53,7 @@ const BTServices = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default BTServices
+export default BTServices;
