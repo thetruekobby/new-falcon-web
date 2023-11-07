35dc8d170c2beb740d8b19cc58d10f1fe881e901
Pius Agyekum <agyekumpius1@gmail.com>
2023-11-07 03:42:15 +0000

    hero and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex flex-col md:flex-row gap-5 items-center">
+      <div className="flex-[1] lg:flex-1 order-last md:order-first">
+        <h1 className="capitalize font-bold text-5xl mb-4">software solutions for ambitious ideas</h1>
+        <h2 className="font-semibold text-[var(--clr-text-secondary)] text-lg mb-4">
+          Unleashing the power of code to transform your vision to reality.
+        </h2>
+        <button className="btn-primary">get in touch</button>
+      </div>
+      <div className="img-outline-right flex-1 order-first md:order-last">
+        <img src="/bg/banner_bg.jpg" alt="" className="w-full" />
+      </div>
+    </div>
   )
 }
 
