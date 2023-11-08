034ea3bc98828c3805b58b740a7ae57e8d9d8974
Pius Agyekum <agyekumpius1@gmail.com>
2023-11-08 03:21:56 +0000

    styles updated

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,11 +1,11 @@
 import ourTeam from "../team.json"
 const Team = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="section-title text-center">Our Team</h1>
+    <div className="container mx-auto pt-10">
+      <h1 className="section-title text-center">Meet Our Team</h1>
       <div className="">
         {ourTeam.teams.map((member, index) => (
-          <div key={index} className="relative bg-gray-100 shadow-[1_1_5px_10px_theme('colors.gray.100')] mt-32 w-[60%] mx-auto px-24 py-10">
+          <div key={index} className="relative bg-gray-100 shadow-[0_0_20px_10px_theme('colors.gray.100')] mt-32 w-[60%] mx-auto px-24 py-10">
             <div className="absolute min-w-[160px] w-[20%] aspect-[1/1.1] -left-20 -top-20 shadow-md rounded overflow-hidden">
               <img src={member.image} alt="w-full h-full object-cover object-center" />
             </div>
